86d1e06d561eb1306ec279e4f43ce4025cb4f171
Noor Alhomsi <nalhomsi@gmail.com>
2021-10-02 23:21:47 -0400

    mock up

---
+++ moto-mern/src/components/About/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import coverImage from "../../assets/cover/cover-image.jpg";
+
+function About() {
+
+  return (
+    <section className="my-5" >
+     
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <div className="my-2">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
+      </p>
+      </div>
+    </section>
+  )
+}
+
+export default About
---
+++ moto-mern/src/components/Nav/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
+
+function Nav() {
+  const  categories = [
+    { name: 'place holder', description: 'Photos of grocery stores, food trucks, and other commercial projects' },
+    { name: 'place holder', description: 'Portraits of people in my life' },
+    { name: 'place holder', description: 'Delicious delicacies' },
+    { name: 'place holder', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' }
+  ];
+
+  const handleClick = () => {
+    console.log("click handled")
+  }
+
+  return (
+    <header data-testid="header" className="flex-row px-1">
+      <h2>
+        <a href="/">
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a href="#about" onClick={() => handleClick()}>
+              About
+            </a>
+          </li>
+          <li className={"mx-2"}>
+            <span onClick={() => handleClick()}>
+              Contact
+            </span>
+          </li>
+          {
+            categories.map((category) => (
+              <li className="mx-1" key={category.name} >
+                <span onClick={() => { handleClick(); }}>
+                 {capitalizeFirstLetter(category.name)}
+                </span>
+              </li>
+            ))
+          }
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
