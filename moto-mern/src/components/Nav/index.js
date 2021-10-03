7478ee8ad4361d333937e78f6869e7b57dd91d22
Noor Alhomsi <nalhomsi@gmail.com>
2021-10-02 23:29:01 -0400

    nav

--- moto-mern/src/components/Nav/index.js
+++ moto-mern/src/components/Nav/index.js
@@ -17,7 +17,7 @@ function Nav() {
     <header data-testid="header" className="flex-row px-1">
       <h2>
         <a href="/">
-          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+          <span role="img" aria-label="car"></span> moto mern
         </a>
       </h2>
       <nav>
