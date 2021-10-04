d52bbc0162f10cd40483efd4762e09c942694b39
BriagasD98 <briagasdavid@yahoo.com>
2021-10-04 18:07:37 -0500

    react-router-dom progress

--- moto-mern/src/App.js
+++ moto-mern/src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import About from './components/About';
+import ShowVroom from './components/ShowVroom';
 
 function App() {
 
   return (
-    <div>
-      <Nav />
-      <main>
-          <About />        
-      </main>
-    </div>
+    <body>
+      <HashRouter>
+        <div>
+          <Nav />
+          <main>
+              <Switch>
+                <Route exact={true} path='/' component={About} />
+                <Route exact path="/about" component={About} />
+                <Route exact path='/showvroom' component={ShowVroom} />
+              </Switch>        
+          </main>
+        </div>
+      </HashRouter>
+    </body>
   );
 }
 
---
+++ moto-mern/src/components/ShowVroom/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ShowVroom() {
+
+    return(
+        <div>
+            <header>
+                <h1>ShowVroom</h1>
+            </header>
+        </div>
+    )
+}
+
+export default ShowVroom;
