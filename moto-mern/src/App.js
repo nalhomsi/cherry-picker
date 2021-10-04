32ed7ebe497d5b4d3ecac523a5ee99212066c36b
Noor Alhomsi <nalhomsi@gmail.com>
2021-10-04 19:31:51 -0400

    neon nav

--- moto-mern/src/App.js
+++ moto-mern/src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import About from './components/About';
+import ShowVroom from './components/ShowVroom';
+import coverImage from "./assets/cover/cover-image.jpg";
 
 function App() {
 
   return (
-    <div>
-      <Nav />
-      <main>
-          <About />        
-      </main>
-    </div>
+    <body>
+      <HashRouter>
+        <div>
+          <Nav />
+          <main>
+          <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+              <Switch>
+                <Route exact={true} path='/' component={About} />
+                <Route exact path="/about" component={About} />
+                <Route exact path='/showvroom' component={ShowVroom} />
+              </Switch>        
+          </main>
+        </div>
+      </HashRouter>
+    </body>
   );
 }
 
--- moto-mern/src/components/Nav/index.js
+++ moto-mern/src/components/Nav/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav() {
@@ -14,31 +15,20 @@ function Nav() {
   }
 
   return (
-    <header data-testid="header" className="flex-row px-1">
-      <nav>
-        <ul className="flex-row">
-          <li className="mx-2">
-            <a href="#about" onClick={() => handleClick()}>
-              About
-            </a>
-          </li>
-          <li className={"mx-2"}>
-            <span onClick={() => handleClick()}>
-              Contact
-            </span>
-          </li>
-          {
-            categories.map((category) => (
-              <li className="mx-1" key={category.name} >
-                <span onClick={() => { handleClick(); }}>
-                 {capitalizeFirstLetter(category.name)}
-                </span>
-              </li>
-            ))
-          }
-        </ul>
-      </nav>
-    </header>
+    <nav className="navbar">
+                <ul className="container navlinks">
+                    <li className="mx-2 navlinks">
+                        <Link to="/about">
+                            About
+                            </Link>
+                    </li>
+                    <li className="mx-2">
+                        <Link to="/showvroom">
+                            ShowVroom
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
   );
 }
 
