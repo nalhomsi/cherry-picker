b30c79a0c2cd35c06130059a2d79414d62c78ab1
BriagasD98 <briagasdavid@yahoo.com>
2021-10-04 19:56:31 -0500

    styling changes

--- moto-mern/src/components/About/index.js
+++ moto-mern/src/components/About/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 // import coverImage from "../../assets/cover/cover-image.jpg";
-
+import Plate from '../../assets/images/texas-plate.png';
 function About() {
 
   return (
     <section>
       <div>
-        <header>
+        <header className="comp-header">
           <h1>About</h1>
         </header>
       </div>
