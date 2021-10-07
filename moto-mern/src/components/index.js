66644eaab778b08543d582f3172d7031fe0c1a92
Noor Alhomsi <nalhomsi@gmail.com>
2021-10-07 15:40:00 -0400

    updates

--- moto-mern/src/components/index.js
+++ moto-mern/src/components/index.js
@@ -1,1 +1,62 @@
-export { default as Contact } from './Contact/index';
+import React, {useState} from "react"
+
+
+function VinVerify() {
+    const [infoData, setInfoData] = useState({
+        vinNumber: '',
+        modelYear: '',
+      });
+const handlesubmit = async (event)=> {
+    event.preventDefault()
+    console.log("handleSubmit")
+    const {vinNumber,modelYear}={...infoData}
+    try {
+const response = await fetch (`https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVin/${vinNumber}?format=json&modelyear=${modelYear}`)
+    if (!response.ok) {
+        throw new Error ("Something's not right")
+    } 
+    const data = await response.json ()
+    console.log(data)
+}
+
+    catch (error) {
+        console.error(error)
+    }
+}
+const handleInputChange=(event)=>{
+    event.preventDefault()
+    const { name, value } = event.target;
+    setInfoData({ ...infoData, [name]: value });
+}
+    return(
+        <center><section>
+            <div>
+                <form onSubmit ={handlesubmit}>
+                  <h4>Enter Model Year   | Enter VIN Number</h4>
+                    <input
+                    type="text"
+                        placeholder ="please enter model year"
+                        name = "modelYear"
+                        onChange={handleInputChange}
+                        value={infoData.modelYearr}
+                    />
+                    <input
+                    type="text"
+                    placeholder ="enter VIN number"
+                    name = "vinNumber"
+                    onChange={handleInputChange}
+                    value={infoData.vinNumber}
+                    />  
+            
+                    <br />
+                    <br />
+                    <button>Check Your Vin</button>
+                </form>
+            </div>
+        </section></center>
+    )
+};
+
+
+
+export default VinVerify;
