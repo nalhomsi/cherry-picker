0a6f6ad7dd1a91046399ec7958780380e6718bfe
BriagasD98 <briagasdavid@yahoo.com>
2021-10-07 23:32:37 -0500

    pictures rendering onto showvroom!

--- moto-mern/src/components/ShowVroom/index.js
+++ moto-mern/src/components/ShowVroom/index.js
@@ -1,6 +1,41 @@
 import React, {useEffect} from "react";
+import img1 from '../../assets/Cars/ac-cobra.jpg';
+import img2 from '../../assets/Cars/austin-healey.jpg';
+import img3 from '../../assets/Cars/bmw-e30.jpg';
+import img4 from '../../assets/Cars/cadillac-deville.jpg';
+import img5 from '../../assets/Cars/chevrolet-c10.jpg';
+import img6 from '../../assets/Cars/ferrari-250gto.jpg';
 
 function ShowVroom() {
+    // Gallery Data
+    let data = [
+        {
+            id: 1,
+            imgSrc: img1,
+        },
+        {
+            id: 2,
+            imgSrc: img2,
+        },
+        {
+            id: 3,
+            imgSrc: img3,
+        },
+        {
+            id: 4,
+            imgSrc: img4,
+        },
+        {
+            id: 5,
+            imgSrc: img5,
+        },
+        {
+            id: 6,
+            imgSrc: img6,
+        },
+    ]
+
+
 // useEffect   (()=>{
 //     console.log ("Hey")
     
@@ -37,6 +72,15 @@ function ShowVroom() {
                 <header className="comp-header">
                     <h1>ShowVroom</h1>
                 </header>
+                <div className="gallery">
+                    {data.map((item, index) => {
+                        return(
+                            <div className="pics" key={index}>
+                                <img src={item.imgSrc} />
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         </section>
     ) 
