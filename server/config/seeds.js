86b4730780efa957739b2406dcf6209542761c23
BriagasD98 <briagasdavid@yahoo.com>
2021-10-09 10:35:02 -0500

    car seed progress

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,2 +1,48 @@
 const db = require('./connection');
 const { User, Car } = require('../models');
+// import car pics
+import img1 from '../../moto-mern/src/assets/Cars/ac-cobra.jpg';
+import img2 from '../../moto-mern/src/assets/Cars/austin-healey.jpg';
+import img3 from '../../moto-mern/src/assets/Cars/bmw-e30.jpg';
+import img4 from '../../moto-mern/src/assets/Cars/cadillac-deville.jpg';
+import img5 from '../../moto-mern/src/assets/Cars/chevrolet-c10.jpg';
+import img6 from '../../moto-mern/src/assets/Cars/ferrari-250gto.jpg';
+import img7 from '../../moto-mern/src/assets/Cars/ford-bronco1.jpg';
+import img8 from '../../moto-mern/src/assets/Cars/ford-f100-red.jpg';
+import img9 from '../../moto-mern/src/assets/Cars/ford-f100.jpg';
+import img10 from '../../moto-mern/src/assets/Cars/ford-mustang.jpg';
+import img11 from '../../moto-mern/src/assets/Cars/jaguar-etype.jpg';
+import img12 from '../../moto-mern/src/assets/Cars/Lancia-Fulvia.jpg';
+import img13 from '../../moto-mern/src/assets/Cars/land-rover-def.jpg';
+import img14 from '../../moto-mern/src/assets/Cars/mercedes-230sl.jpg';
+import img15 from '../../moto-mern/src/assets/Cars/mercedes-300cd.jpg';
+import img16 from '../../moto-mern/src/assets/Cars/mercedes-300sl.jpg';
+import img17 from '../../moto-mern/src/assets/Cars/mercedes-500sl.jpg';
+import img18 from '../../moto-mern/src/assets/Cars/mini-cooper.jpg';
+import img19 from '../../moto-mern/src/assets/Cars/pontiac-gto.jpg';
+import img20 from '../../moto-mern/src/assets/Cars/porsche-911-back.jpg';
+import img21 from '../../moto-mern/src/assets/Cars/porsche-911.jpg';
+import img22 from '../../moto-mern/src/assets/Cars/shelby-cobra.jpg';
+import img23 from '../../moto-mern/src/assets/Cars/triumph-tr6.jpg';
+import img24 from '../../moto-mern/src/assets/Cars/vw-beetle-or.jpg';
+import img25 from '../../moto-mern/src/assets/Cars/vw-beetle-red.jpg';
+import img26 from '../../moto-mern/src/assets/Cars/toyota-fj40.jpg';
+
+db.once('open', async () => {
+    await Car.deleteMany();
+
+    const cars = await Car.insertMany([
+        {
+            year: '1962',
+            make: 'AC',
+            model: 'Cobra',
+            image: ''
+        },
+        {
+            year: '',
+            make: '',
+            model: '',
+            image: ''
+        },
+    ])
+})
