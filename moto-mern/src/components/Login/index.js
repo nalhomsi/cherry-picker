3810ae7a06e8459a099a65b9367c47c2d184eeaa
BriagasD98 <briagasdavid@yahoo.com>
2021-10-09 13:53:11 -0500

    login form styling changes

--- moto-mern/src/components/Login/index.js
+++ moto-mern/src/components/Login/index.js
@@ -2,31 +2,60 @@ import React, { useState } from "react";
 
 const Login=()=>{
         return (
-               <center><form>
-                <h4>Sign In</h4>
+               <div id="loginbody">
+                   <form>
+                    <h2>Log In</h2>
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
-
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    <div className="form-group">
+                        <label>Email address</label>
+                        <input type="email" className="form-control" placeholder="Enter email" />
                     </div>
-                </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                <h5 className="forgot-password text-right">
-                     <small><a href="#">Forgot password?</a></small>
-                </h5>
-            </form></center>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input type="password" className="form-control" placeholder="Enter password" />
+                    </div>
+
+                    <div className="form-group">
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                        </div>
+                    </div>
+
+                    <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                    <h5 className="forgot-password text-right">
+                        <small><a href="#">Forgot password?</a></small>
+                    </h5>
+                </form>
+                <form>
+                    <h2>Sign Up</h2>
+                    
+                    <div className="form-group">
+                        <label>Full Name</label>
+                        <input type="name" className="form-control" placeholder="Enter full name" />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Email address</label>
+                        <input type="email" className="form-control" placeholder="Enter email" />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input type="password" className="form-control" placeholder="Enter password" />
+                    </div>
+
+                    <div className="form-group">
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                        </div>
+                    </div>
+
+                    <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                </form>
+            </div>
            
         );
 }
