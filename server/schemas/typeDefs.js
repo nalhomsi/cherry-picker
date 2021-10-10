11b59cf23e2683030cd6e89cac30d1005f960628
weinge <wein.geoffrey@gmail.com>
2021-10-10 16:11:12 -0500

    adjusted typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,33 +1,32 @@
-
-const {gql} = require ("apollo-server-express")
+const { gql } = require('apollo-server-express');
 const typeDefs = gql`
-  type Car {
-    _id: ID
-    make: String
-    model: String
-    year: String
-  }
-type Auth {
-  token:ID!
-  me:User
-}
-  type Query {
-    cars: [Car]
-    car(make: String!): Car
+	type User {
+		_id: ID!
+		username: String!
+		email: String!
+	}
 
-  }
-  
-  type Mutation {
-    login (email:String!, password: String!): Auth
-    addUser (username:String!, email: String!, password: String!): Auth
-  }
+	type Car {
+		_id: ID
+		make: String
+		model: String
+		year: String
+	}
 
-  type User {
-    _id:ID!
-    username: String!
-    email: String!
-  }
+	type Auth {
+		token: ID!
+		me: User
+	}
+
+	type Query {
+		cars: [Car]
+		car(make: String!): Car
+	}
+
+	type Mutation {
+		login(email: String!, password: String!): Auth
+		addUser(username: String!, email: String!, password: String!): Auth
+	}
 `;
 
 module.exports = typeDefs;
-
