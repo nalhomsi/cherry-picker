52ff1e6cfecdc89879a1447645be38af6cfbbbe5
BriagasD98 <briagasdavid@yahoo.com>
2021-10-10 11:24:45 -0500

    server fix

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
 
 const {typeDefs, resolvers} = require('./schemas');
-//const {authMiddleware} = require('./utils/auth');
+const {authMiddleware} = require('./utils/auths');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
