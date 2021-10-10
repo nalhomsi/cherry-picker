88c8e8235819d61aaef99dd9560d9e8c0eea4ac3
fbenjaminmorris <83195283+fbenjaminmorris@users.noreply.github.com>
2021-10-10 18:48:41 -0400

    update

--- moto-mern/src/components/MyGarage/index.js
+++ moto-mern/src/components/MyGarage/index.js
@@ -16,4 +16,30 @@ function MyGarage() {
     )
 }
 
+var filesForDownload = [];
+filesForDownload( { path: "'../../assets/Cars/ford-mustang.jpg'", name: "ford-mustang.jpg" } );
+filesForDownload( { path: "/path/file2.jpg", name: "file2.jpg" } );
+filesForDownload( { path: "/path/file3.png", name: "file3.png" } );
+filesForDownload( { path: "/path/file4.txt", name: "file4.txt" } );
+
+$jq('input.downloadAll').click( function( e )
+{
+    e.preventDefault();
+
+    var temporaryDownloadLink = document.createElement("a");
+    temporaryDownloadLink.style.display = 'none';
+
+    document.body.appendChild( temporaryDownloadLink );
+
+    for( var n = 0; n < filesForDownload.length; n++ )
+    {
+        var download = filesForDownload[n];
+        temporaryDownloadLink.setAttribute( 'href', download.path );
+        temporaryDownloadLink.setAttribute( 'download', download.name );
+
+        temporaryDownloadLink.click();
+    }
+
+    document.body.removeChild( temporaryDownloadLink );
+} );
 export default MyGarage;
